27ec2860279957980bcf34fc66288f7cdca9f344
Rulerlunae18 <sivenukolga@gmail.com>
2025-11-24 04:39:53 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,16 @@
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+
   server: {
     host: true,
     port: 5173,
     strictPort: true,
-    hmr: {
-      host: "udubs-frontend.onrender.com",
-    },
   },
+
   preview: {
     allowedHosts: [
       "udubs-frontend.onrender.com"
