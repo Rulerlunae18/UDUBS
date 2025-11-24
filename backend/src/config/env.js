8c77a0872da1fe83dfaf9f3a6611c0ad1d4bd5f2
Rulerlunae18 <sivenukolga@gmail.com>
2025-11-25 01:45:24 +0200

    Update env.js

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 let frontend = process.env.FRONTEND_ORIGIN || 'http://localhost:5173';
 let backend  = process.env.BACKEND_URL      || 'http://localhost:3000';
-let uploadDir = process.env.UPLOAD_DIR      || 'uploads';
 
 frontend = frontend.replace(/\/$/, "");
 backend  = backend.replace(/\/$/, "");
@@ -12,6 +11,5 @@ const backendUrl = (path = "") => backend + path;
 module.exports = {
     backend,
     frontend,
-    backendUrl,
-    uploadDir,
+    backendUrl
 };
